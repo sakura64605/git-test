7841fcfbcb66f028c69016d84fcc5c6384d7b958
DengHongJie <1613364605@qq.com>
2025-01-22 11:19:48 +0800

    third update

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("Hello World1");
+        System.out.println("Hello World2");
+        System.out.println("Hello World3");
     }
 }
